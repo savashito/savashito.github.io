308779b64ec4d7103ea5092d25341fa341d3a849
chiara1001 <124921969+chiara1001@users.noreply.github.com>
2025-08-06 14:11:18 +0200

    added farm id to dry milling

--- dry_milling.js
+++ dry_milling.js
@@ -22,6 +22,8 @@ document.getElementById('dry_millingForm').addEventListener('submit', function(e
     const action_weight = document.getElementById('weight').value;
     const action_moisture = document.getElementById('moisture').value;
     const gps_location = document.getElementById('gps').value;
+     const farm_id = document.getElementById("farm").value;
+    console.log("farm_id ", farm_id)
     if(!key){
       alert('Please sign up first.');
     }
@@ -40,7 +42,7 @@ document.getElementById('dry_millingForm').addEventListener('submit', function(e
   const urlParams = new URLSearchParams(window.location.search);
   const nfc_id = urlParams.get("nfc_id");
   const action_type = 'Dry_Milling';
-  const farm_id = '1';
+
   // const ico_number = 'jojo';
   const action_variety_process = null;
   const action_date = dry_millingStartDate;
@@ -50,7 +52,7 @@ document.getElementById('dry_millingForm').addEventListener('submit', function(e
     nfc_id,
     actor_id,
     action_type,
-    farm_id: '1',
+    farm_id: farm_id,
     // icn_number,
     action_variety_process,
     action_date,
